356411a66eb37dcef75575ea2a74e1a2c70aaed5
Joshua Chen <chen.joshua98@gmail.com>
2024-04-21 00:46:33 +0200

    restructuring

--- projects/plotlyCharts.js
+++ projects/plotlyCharts.js
@@ -1,8 +1,38 @@
+
+
 document.addEventListener('DOMContentLoaded', (event) => {
+    // Plot for 'lkrsstats'
     fetch('other/lakers_box_stats.json')
         .then(response => response.json())
         .then(data => {
             Plotly.newPlot('lkrsstats', data.data, data.layout);
         });
-    // Repeat for other charts
+
+    // Plot for 'myDiv2'
+    fetch('other/lebron_season_stats.json')
+        .then(response => response.json())
+        .then(data => {
+            Plotly.newPlot('myDiv2', data.data, data.layout);
+        });
+
+    // Plot for 'myDiv3'
+    fetch('other/lebron_point_breakdown.json')
+        .then(response => response.json())
+        .then(data => {
+            Plotly.newPlot('myDiv3', data.data, data.layout);
+        });
+
+    // Plot for 'myDiv1'
+    fetch('other/lebron_won_lost.json')
+        .then(response => response.json())
+        .then(data => {
+            Plotly.newPlot('myDiv1', data.data, data.layout);
+        });
+
+    // Plot for 'myDiv'
+    fetch('other/lebron_home_away.json')
+        .then(response => response.json())
+        .then(data => {
+            Plotly.newPlot('myDiv', data.data, data.layout);
+        });
 });
